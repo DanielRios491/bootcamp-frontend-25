d9b2eb6d57504ef09f20ac7438144bb60bd2fd7f
DanielRiosLopez <Daniel.Rios@jala.university>
2025-05-18 17:07:26 -0400

    Improved to use the Seat class

--- CinemaOpp.js
+++ CinemaOpp.js
@@ -2,28 +2,44 @@ class Cinema {
     seats;
 
     constructor(rowN, seatN) {
-        this.seats = Array.from(Array(rowN), (x) => x = new Array(seatN).fill("available"));
+        // this.seats = Array.from(Array(rowN), (x) => x = new Array(seatN).fill(new Seat(rowN, seatN, "available")));
+        this.seats = Array.from(Array(rowN), (rowElement, rowIndex) =>
+            Array.from(Array(seatN), (seatElement, seatIndex) =>
+                new Seat(rowIndex, seatIndex, "available")
+            )
+        );
     }
 
 
     showSeatStatus() {
-        console.table(this.seats)
+        console.table( this.seats.map((row, index) => {
+            return row.map((seat, i) => {
+                return seat.state;
+            });
+        }))
     };
 
     reserveSeat(row, seat) {
-        if (this.seats[row][seat] === "available") {
-            this.seats[row][seat] = "held"
+        if (this.seats[row][seat].state === "available") {
+            this.seats[row][seat].state = "held"
             console.log(`Row ${row} and seat ${seat} reserved`);
         }else{
             console.log(`NOT AVAILABLE! Reserving was not possible in row ${row} and seat ${seat}`);
         }
     };
+
 }
 
 class Seat {
     row;
     seat;
     state;
+    
+    constructor(row, seat, state) {
+        this.row = row;
+        this.seat = seat;
+        this.state = state;
+    }
 }
 
 
@@ -32,11 +48,11 @@ function cinemaTestFunction() {
 
     cinema.showSeatStatus();
 
-    cinema.reserveSeat(3,5);
+    cinema.reserveSeat(4,6);
 
     cinema.showSeatStatus();
 
-    cinema.reserveSeat(3,5);
+    cinema.reserveSeat(4,6);
 }
 
 cinemaTestFunction()
