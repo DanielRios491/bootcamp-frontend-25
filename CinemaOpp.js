d1e8f5410b955ab320159385e7133b42a9f82fde
DanielRiosLopez <Daniel.Rios@jala.university>
2025-05-18 15:36:32 -0400

    Challenge solved

--- CinemaOpp.js
+++ CinemaOpp.js
@@ -12,28 +12,31 @@ class Cinema {
 
     reserveSeat(row, seat) {
         if (this.seats[row][seat] === "available") {
-            this.seats[row][seat] = "unavailable"
-            console.log(`Row ${row} and seat ${seat} held`);
+            this.seats[row][seat] = "held"
+            console.log(`Row ${row} and seat ${seat} reserved`);
         }else{
-            console.log(`Row ${row} and seat ${seat} unavailable`);
+            console.log(`NOT AVAILABLE! Reserving was not possible in row ${row} and seat ${seat}`);
         }
     };
 }
 
 class Seat {
-    row = ["AA","AB","AC","A","B","C","D"];
+    row;
     seat;
     state;
-    
-
 }
 
-const cinema = new Cinema(5, 10);
 
-cinema.showSeatStatus();
+function cinemaTestFunction() {
+    const cinema = new Cinema(5, 10);
 
-cinema.reserveSeat(3,5);
+    cinema.showSeatStatus();
 
-cinema.showSeatStatus();
+    cinema.reserveSeat(3,5);
 
-cinema.reserveSeat(3,5);
+    cinema.showSeatStatus();
+
+    cinema.reserveSeat(3,5);
+}
+
+cinemaTestFunction()
