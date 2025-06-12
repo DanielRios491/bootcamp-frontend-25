289ac30a9fefc8a33e3ffe7455e4258173081664
DanielRiosLopez <Daniel.Rios@jala.university>
2025-06-12 16:56:25 -0400

    feature: created components to the functionality

--- src/components/page/WeatherPage.jsx
+++ src/components/page/WeatherPage.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+
 export default function WeatherPage(params) {
-    return <p>Hola World</p>;
+    const {textValue,setTextValue} = useState()
+    const handleChange = (e) => {
+        console.log(e.target.value)
+        setTextValue(e.target.value)
+    }
+
+    return (
+        <>
+        <input type="text" value={textValue} onChange={handleChange}/>
+        <button onClick={() => setTextValue(d => d + 1)}>
+            Forzar re-render
+        </button>
+        </>
+    );
 }
