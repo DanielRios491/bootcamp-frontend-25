fd442f706f85f772378b07c4f45645aca65511d1
DanielRiosLopez <Daniel.Rios@jala.university>
2025-07-14 13:23:52 -0400

    second example added

--- examplePromise.js
+++ examplePromise.js
@@ -12,3 +12,32 @@ const promise = () => fetch("https://pokeapi.co/api/v2/pokemon/ditto")
 
 
 console.log(promise())
+
+//second Example
+function tareaSimple() {
+  return new Promise((resolve, reject) => {
+    const success = true;
+    setTimeout(() => {
+      if (success) {
+        resolve("Task has been completed without issues");
+      } else {
+        reject("Have an issue occured");
+      }
+    }, 1000);
+  });
+}
+
+tareaSimple()
+  .then((mensaje) => {
+    console.log("THEN 1:", mensaje);
+    return "Result processed in THEN 2";
+  })
+  .then((nuevoMensaje) => {
+    console.log("THEN 2:", nuevoMensaje);
+  })
+  .catch((error) => {
+    console.log("CATCH:", error);
+  })
+  .finally(() => {
+    console.log("FINALLY: always working");
+  });
